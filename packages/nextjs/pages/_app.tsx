3a214d0dc6e2bc629a5337d9c59f709ca1b32077
solidoracle <105349716+solidoracle@users.noreply.github.com>
2023-04-11 09:34:11 +0200

    init (#6)

---
+++ packages/nextjs/pages/_app.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import NextNProgress from "nextjs-progressbar";
+import { Toaster } from "react-hot-toast";
+import { WagmiConfig } from "wagmi";
+import { Footer } from "~~/components/Footer";
+import { Header } from "~~/components/Header";
+import { BlockieAvatar } from "~~/components/scaffold-eth";
+import { useEthPrice } from "~~/hooks/scaffold-eth";
+import { useAppStore } from "~~/services/store/store";
+import { wagmiClient } from "~~/services/web3/wagmiClient";
+import { appChains } from "~~/services/web3/wagmiConnectors";
+import "~~/styles/globals.css";
+
+const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
+  const price = useEthPrice();
+  const setEthPrice = useAppStore(state => state.setEthPrice);
+
+  useEffect(() => {
+    if (price > 0) {
+      setEthPrice(price);
+    }
+  }, [setEthPrice, price]);
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <NextNProgress />
+      <RainbowKitProvider chains={appChains.chains} avatar={BlockieAvatar}>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="relative flex flex-col flex-1">
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </div>
+        <Toaster />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+};
+
+export default ScaffoldEthApp;
